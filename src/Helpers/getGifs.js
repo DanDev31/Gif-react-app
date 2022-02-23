abb0a1595c34ea6c8d7f3bed4dd99cbc6c358c10
Daniel Sánchez <juandaniel.25@outlook.com>
2022-02-23 18:52:32 -0500

    fetch request fixed

--- src/Helpers/getGifs.js
+++ src/Helpers/getGifs.js
@@ -2,7 +2,7 @@
 
 export const getGifs = async ( category ) =>{
 
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI( category )}&limit=10&api_key=suP44AJAXv6bck79AUycRE7KBNRfC0yE`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI( category )}&limit=10&api_key=suP44AJAXv6bck79AUycRE7KBNRfC0yE`;
     const response = await fetch(url);
     const { data } = await response.json();
   
